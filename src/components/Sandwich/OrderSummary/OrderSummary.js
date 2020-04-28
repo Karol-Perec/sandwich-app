6cf4e51b4b224264e6cd8149565a3c744eb7f0fb
karol.perec@gmail.com <karol.perec@gmail.com>
2020-04-28 03:03:22 +0200

    Add ordering functionality

---
+++ src/components/Sandwich/OrderSummary/OrderSummary.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map(
+    (ingredientKey) => {
+      return (
+        <li key={ingredientKey}>
+          <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>:{' '}
+          {props.ingredients[ingredientKey]}
+        </li>
+      );
+    }
+  );
+  return (
+    <>
+      <h3>Your Order</h3>
+      <p>Your best personalized sandwich includes: </p>
+      <ul>{ingredientSummary}</ul>
+      <p>Proceed to Checkout?</p>
+      <Button buttonType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button buttonType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </>
+  );
+};
+
+export default orderSummary;
--- src/containters/SandwichBuilder/SandwichBuilder.js
+++ src/containters/SandwichBuilder/SandwichBuilder.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
-import Sandwich from './../../components/Sandwich/Sandwich';
+import Sandwich from '../../components/Sandwich/Sandwich';
+import BuildControls from '../../components/Sandwich/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Sandwich/OrderSummary/OrderSummary';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  ham: 1.3,
+  bacon: 0.7,
+};
 
 class SandwichBuilder extends Component {
   state = {
@@ -10,13 +20,80 @@ class SandwichBuilder extends Component {
       cheese: 0,
       ham: 0,
     },
+    totalPrice: 4,
+    purchaseable: false,
+    purchasing: false,
+  };
+
+  updatePurchaseState(ingredients) {
+    const ingredientsSum = Object.values(ingredients).reduce(
+      (sum, el) => sum + el,
+      0
+    );
+    this.setState({ purchaseable: ingredientsSum > 0 });
+  }
+
+  addIngredient = (type) => {
+    const updatedIgredients = {
+      ...this.state.ingredients,
+    };
+    updatedIgredients[type] = this.state.ingredients[type] + 1;
+    const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+    this.setState({ ingredients: updatedIgredients, totalPrice: newPrice });
+    this.updatePurchaseState(updatedIgredients);
+  };
+
+  removeIngredient = (type) => {
+    const updatedIgredients = {
+      ...this.state.ingredients,
+    };
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    updatedIgredients[type] = oldCount - 1;
+    const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+    this.setState({ ingredients: updatedIgredients, totalPrice: newPrice });
+    this.updatePurchaseState(updatedIgredients);
+  };
+
+  purchase = () => {
+    this.setState({ purchasing: true });
+  };
+
+  purchaseCancel = () => {
+    this.setState({ purchasing: false });
+  };
+
+  purchaseContinue = () => {
+    alert('Purchased');
   };
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients,
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
       <>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancel}>
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancel}
+            purchaseContinued={this.purchaseContinue}
+          />
+        </Modal>
         <Sandwich ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls
+          ingredientAdded={this.addIngredient}
+          ingredientRemoved={this.removeIngredient}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+          purchaseable={this.state.purchaseable}
+          ordered={this.purchase}
+        />
       </>
     );
   }
