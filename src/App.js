3d9ba03dcb5d452dbc242ea989c121fef2f7605c
karol.perec@gmail.com <karol.perec@gmail.com>
2020-05-03 23:12:01 +0200

    Move Layout to components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Layout from './components/Layout/Layout';
+import Layout from './containters/Layout/Layout';
 import SandwichBuilder from './containters/SandwichBuilder/SandwichBuilder';
 
 function App() {
