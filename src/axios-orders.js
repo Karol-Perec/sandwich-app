ebeddf0c9de51c655402b69a52026de3760ce5cd
Karol Perec <karol.perec@gmail.com>
2020-12-14 19:12:01 +0100

    env variable api

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://sandwich-app-37958.firebaseio.com/',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default instance;
